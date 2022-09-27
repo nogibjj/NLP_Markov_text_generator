bde5dad70862e4f33dc78008bf4724854a7f28d9
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-09-27 15:54:57 +0000

    add MarkKov_generator_readme.txt

--- Markov_generator.py
+++ Markov_generator.py
@@ -1,6 +1,7 @@
 """Markov Text Generator.
 
 Patrick Wang, 2021
+Modified by Scott Lai, 2022
 
 Resources:
 Jelinek 1985 "Markov Source Modeling of Text Generation"
@@ -38,5 +39,33 @@ def test_generator():
     ]
 
 
+def test_generator_2():
+    """Test Markov text generator."""
+    corpus = nltk.word_tokenize(
+        nltk.corpus.gutenberg.raw("austen-sense.txt").lower())
+
+    words = finish_sentence(
+        ["she", "was", "not", "that"],
+
+        2,
+        corpus,
+        deterministic=False,
+    )
+    print(words)
+    # assert words == ["she", "was", "not", "in", "the", "world", "."] or words == [
+    #     "she",
+    #     "was",
+    #     "not",
+    #     "in",
+    #     "the",
+    #     "world",
+    #     ",",
+    #     "and",
+    #     "the",
+    #     "two",
+    # ]
+
+
 if __name__ == "__main__":
     test_generator()
+    test_generator_2()
